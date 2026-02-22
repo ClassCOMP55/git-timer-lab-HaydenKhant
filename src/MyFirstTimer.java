5692a8114c92b638e71301aaab2843d32559368f
Hayden Khant <t_khant@u.pacific.edu>
2026-02-21 18:38:07 -0800

    The 3 things the Timer needs are: The "import javax.swing.*" class, the delay in milliseconds and the listener object.

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,20 +1,36 @@
 import acm.graphics.*;
 import acm.program.*;
+import javax.swing.*;
+import java.awt.event.*;
 
-public class MyFirstTimer extends GraphicsProgram {
+public class MyFirstTimer extends GraphicsProgram implements ActionListener{
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
+	private int numTimes = 0;
 	private GLabel myLabel;
+	private Timer t;
 
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
 		requestFocus();
 	}
 	
+	public void actionPerformed(ActionEvent e) {
+		myLabel.move(5, 0);
+		numTimes++;
+		myLabel.setLabel("times called? " + numTimes);
+		if (numTimes == 10) {
+			t.stop();
+		}
+	}
+	
 	public void run() {
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+		t  = new Timer(1000, this);
+		t.setInitialDelay(3000);
+		t.start();
 	}
 	
 	public static void main(String[] args) {
